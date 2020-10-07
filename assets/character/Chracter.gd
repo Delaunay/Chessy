82b06578a87bd72d973b56e887b7d4fbb3f6c8fa
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2020-10-06 20:20:07 -0400

    Add range preview

--- assets/character/Chracter.gd
+++ assets/character/Chracter.gd
@@ -19,29 +19,38 @@ func display(viewport, camera):
 	
 
 func _physics_process(delta):
-	pass
+	var dir = Vector3(0, -1, 0)
+	
+	# Get the camera's transform basis, but remove the X rotation such
+	# that the Y axis is up and Z is horizontal.
+	# var cam_basis = $Camera.global_transform.basis
+	# var basis = cam_basis.rotated(cam_basis.x, -cam_basis.get_euler().x)
+	# dir = basis.xform(dir)
+	
+	# Limit the input to a length of 1. length_squared is faster to check.
+	if dir.length_squared() > 1:
+		dir /= dir.length()
 
-#	# Apply gravity.
-#	velocity.y += delta * gravity
-#
-#	# Using only the horizontal velocity, interpolate towards the input.
-#	var hvel = velocity
-#	hvel.y = 0
-#
-#	var acceleration
-#	if dir.dot(hvel) > 0:
-#		acceleration = ACCELERATION
-#	else:
-#		acceleration = DECELERATION
-#
-#	hvel = hvel.linear_interpolate(target, acceleration * delta)
-#
-#	# Assign hvel's values back to velocity, and then move.
-#	velocity.x = hvel.x
-#	velocity.z = hvel.z
-#	velocity = move_and_slide(velocity, Vector3.UP)
-#
-#	# Jumping code. is_on_floor() must come after move_and_slide().
-#	if is_on_floor() and Input.is_action_pressed("jump"):
-#		velocity.y = JUMP_SPEED
-#
+	# Apply gravity.
+	velocity.y += delta * gravity
+
+	# Using only the horizontal velocity, interpolate towards the input.
+	var hvel = velocity
+	hvel.y = 0
+
+	var target = dir * MAX_SPEED
+	var acceleration
+	
+	if dir.dot(hvel) > 0:
+		acceleration = ACCELERATION
+	else:
+		acceleration = DECELERATION
+
+	hvel = hvel.linear_interpolate(target, acceleration * delta)
+
+	# Assign hvel's values back to velocity, and then move.
+	velocity.x = hvel.x
+	velocity.z = hvel.z
+	velocity.y = hvel.y
+	velocity = move_and_slide(velocity, Vector3.UP)
+
--- assets/tactician.gd
+++ assets/tactician.gd
@@ -9,6 +9,40 @@ var unit_selected = false
 
 onready var character_asset = preload("res://assets/character/Character.tscn")
 
+var base_axial_direction = [
+	Vector3(+1, 0, 0), Vector3(+1, 0, -1), Vector3(0, 0, -1), 
+	Vector3(-1, 0, 0), Vector3(-1, 0, +1), Vector3(0, 0, +1), 
+]
+
+
+func neigboors():
+	# generates list of immediate neighboors
+	var neighs = []
+	
+	for dir in base_axial_direction:
+		for i in [1, 0, -1]:
+			neighs.append(dir + Vector3(0, i, 0))
+			
+	return neighs
+
+
+var axial_direction = neigboors()
+
+
+func highlight_pos_circle(cell):
+	# highlight an area around the character for testing purposes
+	for dir in base_axial_direction:
+		# check current level then one higher then one lower
+		for i in [0, 1, -1]:
+			var neigh = cell + dir + Vector3(0, i, 0)
+
+			if grid_map.get_cell_item(neigh.x, neigh.y, neigh.z) == GridMap.INVALID_CELL_ITEM:
+				continue
+				 
+			grid_map.set_cell_data(neigh.x, neigh.y, neigh.z, Color(1, 0, 0, 0))
+			# we found one tile in that direction no need to check other levels
+			break
+
 
 func create_character(pos):
 	#  insert a new charater on the map
@@ -21,19 +55,27 @@ func create_character(pos):
 	$Units.add_child(instance)
 	var translate = world_pos + Vector3(0, grid_map.get_cell_size().y, 0)
 	instance.translate(translate / instance.scale)
-	
+
+	# highlight_pos_circle(map_pos)
 	print("pos", pos)
+	return map_pos
 
 
 func add_units():
+	# TODO: move this when the grid map is loaded
 	var positions = [
-		Vector3(-1.5, 1, -2.598076),
-		Vector3(1.5, 1, -0.866025),
-		Vector3(-1.5, 0, 0.866025)
+		Vector3(0, 0, 0),
+		Vector3(5, 0, 6),
+		Vector3(5, 0, 0)
 	]
 	
+	var d = 0
 	for p in positions:
-		create_character(p)
+		var cell = create_character(p)
+		d += 1
+		
+		if d == 1:
+			show_unit_range(cell, 2)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -66,6 +108,7 @@ func snap_to_grid(pos, offset=1):
 #	pass
 
 func display(obj = null):
+	# TODO move this to the UnitInfo script
 	var display_container = $HUD/UnitInfo/VBoxContainer/Preview/Viewport/DisplayWorld/Container
 
 	for child in display_container.get_children():
@@ -83,7 +126,10 @@ func select_object(event, pos, collision):
 	var obj = null
 
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed:
-
+		# TODO: move this logic into an editor plugin to place entities
+		# on the grid map
+		# create_character(pos)
+		
 		if collision.collider is GridMap:
 			selection = null
 			unit_selected = false
@@ -99,6 +145,43 @@ func select_object(event, pos, collision):
 		display(obj)
 
 
+func show_unit_range(cell, unit_range):
+	# breadth-first search given our unit range
+	if grid_map.get_cell_item(cell.x, cell.y, cell.z) == GridMap.INVALID_CELL_ITEM:
+		return null
+
+	# we keep the depth because tile types will influence the range
+	var pending = [[0, cell]]
+	while len(pending) > 0:
+		var data = pending.pop_front()
+
+		var d = data[0]
+		var c = data[1]
+
+		for dir in base_axial_direction:
+			var highlight = null
+
+			for i in [-1, 0, 1]:
+				var neigh = c + dir + Vector3(0, i, 0)
+
+				if grid_map.get_cell_item(neigh.x, neigh.y, neigh.z) == GridMap.INVALID_CELL_ITEM:
+					continue
+				
+				highlight = neigh
+
+				if d + 1 < unit_range:
+					pending.append([d + 1, highlight])
+				
+				grid_map.set_cell_data(highlight.x, highlight.y, highlight.z, Color(1, 0, 0, 0))
+	
+	return
+
+
+# TODO: move this to the GridMap Script
+func select_move_path():
+	pass
+
+
 func _input(event):
 	var collision = null
 	var pos = null
@@ -108,17 +191,14 @@ func _input(event):
 		collision = $Camera.select_object(event.position)
 
 	if collision:
-		var offset = 1
-
 		if collision.collider is GridMap:
 			pos = collision.position
 		else:
 			# Object should have a position which is compatible with 
 			# our grid system so we know that this will get us a tile
 			pos = collision.collider.get_global_transform().origin
-			offset = 1
 
-		pos = snap_to_grid(pos, offset)
+		pos = snap_to_grid(pos)
 
 	if pos != null:
 		$Cursor.translate(pos - $Cursor.get_global_transform().origin)
