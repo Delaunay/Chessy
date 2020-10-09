dcab689c64281f0fae84226aa10cabafa6138a11
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2020-10-08 20:35:29 -0400

    tweaks

--- assets/character/HealthBar.gd
+++ assets/character/HealthBar.gd
@@ -9,7 +9,7 @@ extends Sprite3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	texture = $Viewport.get_texture()
-	set_value(25)
+	set_value(75)
 
 
 var red = Color(1, 0, 0, 1)
@@ -17,22 +17,27 @@ var yellow = Color(1, 1, 0, 1)
 var green = Color(0, 1, 0, 1)
 
 
-func set_value(val):
-	$Viewport/ProgressBar.value = val
-	
-	# create a color gradient red 0 50-> yellow 50 -> green 50 - 100
+func color_gradient(val):
+	# create a color gradient red 0 50 -> yellow 50 -> green 50 - 100
 	var color = null
 	var v = float(val)
-	var pos = v / 50.0
-	var neg = 1.0 - pos
-	
+	var top_color = yellow
+	var bot_color = red
+
 	if val > 50:
 		v -= 50
-		color = green * pos + yellow * neg
-	else:
-		color = yellow * pos + red * neg
+		top_color = green
+		bot_color = yellow
 	
-	modulate = color
+	var pos = v / 50.0
+	var neg = 1.0 - pos
+
+	return top_color * pos + bot_color * neg
+
+
+func set_value(val):
+	$Viewport/ProgressBar.value = val
+	modulate = color_gradient(val)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
