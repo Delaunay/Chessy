8e5cdfefbecf2174bc3fea91783c530f4d2ab0db
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2020-10-30 22:44:48 -0400

    tidy things up

--- assets/tile/tile.gd
+++ assets/tile/tile.gd
@@ -1,14 +1,27 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+
+var mat_cache = {}
+
+func get_material(kind):
+	var result = mat_cache.get(kind)
+
+	if result == null:
+		result = $MeshInstance.get_surface_material(0).duplicate()
+		# result.set_shader_param("tex_frg_2", prev_direction.get(d1))
+		# result.set_shader_param("tex_frg_3", dest_direction.get(d2))
+		mat_cache[kind] = result
+	
+	return result
+
+
+func __init__(kind):
+	$MeshInstance.set_surface_material(0, get_material(kind))
 
 
 func select():
@@ -18,7 +31,3 @@ func select():
 func unselect():
 	var mat = $MeshInstance.get_material()
 	mat.set_shader_param("selected", false)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
